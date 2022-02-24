887b739a1b8b1962932594a2189c7d6898798df9
RabynCollab <rabyn900@gmail.com>
2022-02-24 17:54:07 +0545

    changes

--- lib/provider/news_provider.dart
+++ lib/provider/news_provider.dart
@@ -3,15 +3,19 @@ import 'package:flutter_project/models/news.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
-final newsProvider = FutureProvider((ref) => NewsProvider().getData());
+final newsProvider = StateNotifierProvider<NewsProvider, List<News>>((ref) => NewsProvider());
 
-class NewsProvider{
+class NewsProvider extends StateNotifier<List<News>>{
+  NewsProvider() : super([]){
+    getData();
+  }
 
-  Future<List<News>> getData() async{
+
+  Future<void> getData() async{
    final dio = Dio();
    try{
      final response = await dio.get('https://free-news.p.rapidapi.com/v1/search', queryParameters: {
-       'q': 'nasa',
+       'q': 'movies',
        'lang': 'en'
      }, options: Options(
        headers: {
@@ -20,14 +24,35 @@ class NewsProvider{
        }
      ));
      final data = (response.data['articles'] as List).map((e) => News.fromJson(e)).toList();
-      return data;
+      state =  data;
    }on DioError catch (err){
     print(err.response);
-    return [];
 
    }
+  }
 
 
+
+
+  Future<void> searchNews({required String query}) async{
+    final dio = Dio();
+    try{
+      state = [];
+      final response = await dio.get('https://free-news.p.rapidapi.com/v1/search', queryParameters: {
+        'q': query,
+        'lang': 'en'
+      }, options: Options(
+          headers: {
+            'x-rapidapi-host': 'free-news.p.rapidapi.com',
+            'x-rapidapi-key': '9cac60b351msh530cc0e2b8d88d2p1f9661jsn2c0049707e46'
+          }
+      ));
+      final data = (response.data['articles'] as List).map((e) => News.fromJson(e)).toList();
+      state =  data;
+    }on DioError catch (err){
+      print(err.response);
+
+    }
   }
 
 
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,85 +3,110 @@ import 'package:flutter/material.dart';
 import 'package:flutter_project/provider/news_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
-
 class HomeScreen extends StatelessWidget {
+
+  final searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
         body: SafeArea(
-          child: Consumer(
-            builder: (context, ref, child){
-              final news = ref.watch(newsProvider);
-              return news.when(
-                  data: (data){
-                    return ListView.builder(
-                        itemCount: data.length,
-                        itemBuilder: (context, index){
-                          final dat = data[index];
-                          return Card(
-                            child: Container(
-                              padding: EdgeInsets.all(7),
-                              height: 195,
-                              width: double.infinity,
-                              child: Row(
-                                children: [
-
-                                  Expanded(
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        Text(dat.title, maxLines: 2, overflow: TextOverflow.ellipsis, style: TextStyle(fontWeight: FontWeight.w500),),
-                                       SizedBox(height: 7,),
-                                        Text(dat.summary, maxLines: 5, overflow: TextOverflow.ellipsis, style: TextStyle(color: Colors.blueGrey),),
-                                        Padding(
-                                          padding: const EdgeInsets.symmetric(vertical: 5),
-                                          child: Text(dat.author,  overflow: TextOverflow.ellipsis,),
-                                        ),
-                                        Text(dat.published_date,  overflow: TextOverflow.ellipsis,),
-                                      ],
+      child: Consumer(builder: (context, ref, child) {
+        final news = ref.watch(newsProvider);
+        return news.isEmpty ? Center(child: CircularProgressIndicator(),) : Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 2, vertical: 10),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: TextFormField(
+                 // controller: searchController,
+                  onFieldSubmitted: (val){
+                    ref.read(newsProvider.notifier).searchNews(query: val);
+                  },
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    hintText: 'search for latest news',
+                    border: OutlineInputBorder()
+                  ),
+                ),
+              ),
+              SizedBox(height: 10,),
+              Container(
+                height: 720,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                    itemCount: news.length,
+                    itemBuilder: (context, index) {
+                      final dat = news[index];
+                      return Card(
+                        child: Container(
+                          padding: EdgeInsets.all(7),
+                          height: 195,
+                          width: double.infinity,
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      dat.title,
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                      style:
+                                      TextStyle(fontWeight: FontWeight.w500),
                                     ),
-                                  ),
-
-
-                                  SizedBox(width: 10,),
-                             dat.media == '' ? Container(
-                               width: 170,
-                               height: 195,
-                               child: ClipRRect(
-                                   borderRadius: BorderRadius.circular(10),
-                                   child: Image.asset('assets/media.png', fit: BoxFit.fill,)),)  :   Container(
-                                    width: 170,
-                                    height: 195,
-                                    child: ClipRRect(
-                                        borderRadius: BorderRadius.circular(10),
-                                        child: CachedNetworkImage(
-                                         imageUrl: dat.media,
-                                          errorWidget: (context, err, child) => Image.asset('assets/media.png'),
-                                          fit: BoxFit.fill,
-                                        ))
-                               ,),
-
-
-                                ],
+                                    SizedBox(
+                                      height: 7,
+                                    ),
+                                    Text(
+                                      dat.summary,
+                                      maxLines: 5,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(color: Colors.blueGrey),
+                                    ),
+                                    Padding(
+                                      padding:
+                                      const EdgeInsets.symmetric(vertical: 5),
+                                      child: Text(
+                                        dat.author,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ),
+                                    Text(
+                                      dat.published_date,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ],
+                                ),
                               ),
-                            ),
-                          );
-                        }
-                    );
-                  },
-                  error: (err, stack){
-                    print(err);
-                    return Center(child: Text('something went wrong'),);
-                  },
-                  loading: () => Center(child: CircularProgressIndicator(
-                    color: Colors.purple,
-                  ),)
-              );
-            }
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Container(
+                                width: 170,
+                                height: 195,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: dat.media == '' ? Image.asset('assets/images/no-image.jpg') : CachedNetworkImage(
+                                    imageUrl: dat.media,
+                                    errorWidget: (context, url, error) =>  Image.asset('assets/images/no-image.jpg'),
+                                    fit: BoxFit.fill,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    }),
+              ),
+            ],
           ),
-        )
-    );
+        );
+      }),
+    ));
   }
 }
