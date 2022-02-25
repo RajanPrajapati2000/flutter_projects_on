fa218f647d69e1848c6fcd63fec61046f83ebbbb
RabynCollab <rabyn900@gmail.com>
2022-02-25 08:50:55 +0545

    changes

--- lib/provider/news_provider.dart
+++ lib/provider/news_provider.dart
@@ -11,33 +11,39 @@ class NewsProvider extends StateNotifier<List<News>>{
   }
 
 
-  Future<void> getData() async{
-   final dio = Dio();
-   try{
-     final response = await dio.get('https://free-news.p.rapidapi.com/v1/search', queryParameters: {
-       'q': 'movies',
-       'lang': 'en'
-     }, options: Options(
-       headers: {
-         'x-rapidapi-host': 'free-news.p.rapidapi.com',
-         'x-rapidapi-key': '9cac60b351msh530cc0e2b8d88d2p1f9661jsn2c0049707e46'
-       }
-     ));
-     final data = (response.data['articles'] as List).map((e) => News.fromJson(e)).toList();
-      state =  data;
-   }on DioError catch (err){
-    print(err.response);
 
-   }
+
+  Future<void> getData() async{
+    final dio = Dio();
+    try{
+      final response = await dio.get('https://free-news.p.rapidapi.com/v1/search', queryParameters: {
+        'q': 'news',
+        'lang': 'en'
+      }, options: Options(
+          headers: {
+            'x-rapidapi-host': 'free-news.p.rapidapi.com',
+            'x-rapidapi-key': '9cac60b351msh530cc0e2b8d88d2p1f9661jsn2c0049707e46'
+          }
+      ));
+     if(response.data['status'] == 'No matches for your search.'){
+        state = [News(summary: '', published_date: '', link: '', author: '', title: 'no_data', media: '')];
+     }else{
+       final data = (response.data['articles'] as List).map((e) => News.fromJson(e)).toList();
+       state = data;
+     }
+
+    }on DioError catch (err){
+      print(err.response);
+
+    }
   }
 
 
 
-
-  Future<void> searchNews({required String query}) async{
+  Future<void> searchNews(String query) async{
     final dio = Dio();
+    state = [];
     try{
-      state = [];
       final response = await dio.get('https://free-news.p.rapidapi.com/v1/search', queryParameters: {
         'q': query,
         'lang': 'en'
@@ -47,13 +53,22 @@ class NewsProvider extends StateNotifier<List<News>>{
             'x-rapidapi-key': '9cac60b351msh530cc0e2b8d88d2p1f9661jsn2c0049707e46'
           }
       ));
-      final data = (response.data['articles'] as List).map((e) => News.fromJson(e)).toList();
-      state =  data;
+      if(response.data['status'] == 'No matches for your search.'){
+        state = [News(summary: '', published_date: '', link: '', author: '', title: 'no_data', media: '')];
+      }else{
+        final data = (response.data['articles'] as List).map((e) => News.fromJson(e)).toList();
+        state = data;
+      }
     }on DioError catch (err){
       print(err.response);
 
     }
   }
+
+
+
+
+
 
 
 
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,112 +1,111 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_project/provider/news_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+
+
 class HomeScreen extends StatelessWidget {
 
-  final searchController = TextEditingController();
+
+ final searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
         body: SafeArea(
-      child: Consumer(builder: (context, ref, child) {
-        final news = ref.watch(newsProvider);
-        return news.isEmpty ? Center(child: CircularProgressIndicator(),) : Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 2, vertical: 10),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: TextFormField(
-                 // controller: searchController,
-                  onFieldSubmitted: (val){
-                    ref.read(newsProvider.notifier).searchNews(query: val);
-                  },
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                    hintText: 'search for latest news',
-                    border: OutlineInputBorder()
-                  ),
-                ),
-              ),
-              SizedBox(height: 10,),
-              Container(
-                height: 720,
-                child: ListView.builder(
-                  shrinkWrap: true,
-                    itemCount: news.length,
-                    itemBuilder: (context, index) {
-                      final dat = news[index];
-                      return Card(
-                        child: Container(
-                          padding: EdgeInsets.all(7),
-                          height: 195,
-                          width: double.infinity,
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
+          child: Consumer(
+              builder: (context, ref, child){
+                final news = ref.watch(newsProvider);
+                return   Column(
+                  children: [
+                    Container(
+                        height: 70,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+                          child: TextFormField(
+                            controller: searchController,
+                            onFieldSubmitted: (val){
+                              ref.read(newsProvider.notifier).searchNews(val);
+                              searchController.clear();
+                            },
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                              hintText: 'Search for news',
+                              border: OutlineInputBorder()
+                            ),
+                          ),
+                        )),
+                 news.isEmpty ? Container(
+                   height: MediaQuery.of(context).size.height - 100,
+                   child: Center(child: CircularProgressIndicator(
+                     color: Colors.purple,
+                   ),),
+                 ) :  news[0].title == 'no_data' ? Container(
+                   margin: EdgeInsets.only(top: 120),
+                   child: Text('No matches for your search.'),)  :  Container(
+                   height: MediaQuery.of(context).size.height - 100,
+                   child: ListView.builder(
+                     shrinkWrap: true,
+                          itemCount: news.length,
+                          itemBuilder: (context, index){
+                            final dat = news[index];
+                            return Card(
+                              child: Container(
+                                padding: EdgeInsets.all(7),
+                                height: 195,
+                                width: double.infinity,
+                                child: Row(
                                   children: [
-                                    Text(
-                                      dat.title,
-                                      maxLines: 2,
-                                      overflow: TextOverflow.ellipsis,
-                                      style:
-                                      TextStyle(fontWeight: FontWeight.w500),
-                                    ),
-                                    SizedBox(
-                                      height: 7,
-                                    ),
-                                    Text(
-                                      dat.summary,
-                                      maxLines: 5,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: TextStyle(color: Colors.blueGrey),
-                                    ),
-                                    Padding(
-                                      padding:
-                                      const EdgeInsets.symmetric(vertical: 5),
-                                      child: Text(
-                                        dat.author,
-                                        overflow: TextOverflow.ellipsis,
+
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Text(dat.title, maxLines: 2, overflow: TextOverflow.ellipsis, style: TextStyle(fontWeight: FontWeight.w500),),
+                                          SizedBox(height: 7,),
+                                          Text(dat.summary, maxLines: 5, overflow: TextOverflow.ellipsis, style: TextStyle(color: Colors.blueGrey),),
+                                          Padding(
+                                            padding: const EdgeInsets.symmetric(vertical: 5),
+                                            child: Text(dat.author,  overflow: TextOverflow.ellipsis,),
+                                          ),
+                                          Text(dat.published_date,  overflow: TextOverflow.ellipsis,),
+                                        ],
                                       ),
                                     ),
-                                    Text(
-                                      dat.published_date,
-                                      overflow: TextOverflow.ellipsis,
-                                    ),
+
+
+                                    SizedBox(width: 10,),
+                                    dat.media == '' ? Container(
+                                      width: 170,
+                                      height: 195,
+                                      child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(10),
+                                          child: Image.asset('assets/media.png', fit: BoxFit.fill,)),)  :   Container(
+                                      width: 170,
+                                      height: 195,
+                                      child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(10),
+                                          child: CachedNetworkImage(
+                                            imageUrl: dat.media,
+                                            errorWidget: (context, err, child) => Image.asset('assets/media.png'),
+                                            fit: BoxFit.fill,
+                                          ))
+                                      ,),
                                   ],
                                 ),
                               ),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              Container(
-                                width: 170,
-                                height: 195,
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(10),
-                                  child: dat.media == '' ? Image.asset('assets/images/no-image.jpg') : CachedNetworkImage(
-                                    imageUrl: dat.media,
-                                    errorWidget: (context, url, error) =>  Image.asset('assets/images/no-image.jpg'),
-                                    fit: BoxFit.fill,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    }),
-              ),
-            ],
+                            );
+                          }
+                      ),
+                 ),
+                  ],
+                );
+              }
           ),
-        );
-      }),
-    ));
+        )
+    );
   }
 }
